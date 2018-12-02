e849d13eeae81c3ff773f9e1b30272f52b607229
himanshu aswal <haswal403@gmail.com>
2018-12-02 14:27:29 +0530

    split the data

--- model.py
+++ model.py
@@ -17,4 +17,7 @@ features_raw = full_data.drop('Survived', axis = 1)
 #preprocess the data
 features = pd.get_dummies(features_raw)
 features = features.fillna(0.0)
-display(features.head())
+display(features.head
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(features, outcomes, test_size=0.2, random_state=42)
