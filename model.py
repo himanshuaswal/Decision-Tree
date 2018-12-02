40ca13062ae9634e1eb4975c12b4b9631a67fc70
himanshu aswal <haswal403@gmail.com>
2018-12-02 14:29:30 +0530

    train the model

--- model.py
+++ model.py
@@ -19,5 +19,9 @@ features = pd.get_dummies(features_raw)
 features = features.fillna(0.0)
 display(features.head
 
+#splitting the dataset
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(features, outcomes, test_size=0.2, random_state=42)
+#Train the model
+new_model= DecisionTreeClassifier(max_depth=,min_samples_leaf=5)
+new_model.fit(X_train,y_train)
